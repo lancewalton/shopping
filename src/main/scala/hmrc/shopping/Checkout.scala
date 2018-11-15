469e10d8a0cc554625709bf4f18d8a834d128d03
lancewalton <lancewalton@mac.com>
2018-11-15 15:54:25 +0000

    Step 2: Refactored the n for m discounting logic in Checkout to eliminate the duplication

--- src/main/scala/hmrc/shopping/Checkout.scala
+++ src/main/scala/hmrc/shopping/Checkout.scala
@@ -8,12 +8,14 @@ object Checkout {
 
   def calculatePrice(items: List[Item]): Price = items.groupBy(identity).mapValues(_.size).foldLeft(Price.zero) { case (total, (item, number)) ⇒
     val itemPrice = item match {
-      case Apple => applePrice * (number / 2 + number % 2)
-      case Orange => orangePrice * (number / 3 * 2 + number % 3)
+      case Apple => nForMDiscountPrice(applePrice, number, 2, 1)
+      case Orange => nForMDiscountPrice(orangePrice, number, 3, 2)
     }
 
     total + itemPrice
   }
+
+  private def nForMDiscountPrice(unitPrice: Price, number: Int, n: Int, m: Int): Price = unitPrice * (number / n * m + number % n)
 }
 
 
